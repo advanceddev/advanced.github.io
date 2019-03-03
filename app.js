b0aa3e40af2ba6f869f00c4e915b31b6a133e596
ivan <sequendnb@gmail.com>
2019-03-03 17:28:38 +0500

    Integrate VueJS

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
-document.getElementById("action_btn").onclick = function() {
-	location.href = "https://github.com/advanceddev";
-};
+'use-strict'
 
-function handleAction() {
-	var text = "https://advanceddev.github.io/advanced.github.io/advanced.css";
-	navigator.clipboard.writeText(text).then(function() {
-		console.log('Async: Copying to clipboard was successful!');
-		alert('Copied!')
-	}, function(err) {
-		console.error('Async: Could not copy text: ', err);
-	});
-}
+Vue.component('action-button', {
+  data: function () {
+    return {
+      toClipboard: 'https://advanceddev.github.io/advanced.github.io/advanced.css',
+      copied: false
+    }
+  },
+  methods: {
+  	handleAction() {
+  		this.copied = true
+  		navigator.clipboard.writeText(this.toClipboard).then(function() {
+  			alert('Copied to clipboard!')
+		}, function(err) {
+			console.error('Async: Could not copy text: ', err);
+		});
+  	}
+  },
+  template: '<button @click="handleAction" class="b1">Copy direct URL to clipboard!</button>'
+})
+
+Vue.component('github-button', {
+  data: function () {
+    return {
+      url: 'https://github.com/advanceddev'
+    }
+  },
+  methods: {
+  	handleRedirect() {
+  		location.href = this.url;
+  	}
+  },
+  template: '<button @click="handleRedirect" class="b2 b-github">Our GitHub</button>'
+})
+
+var app = new Vue({
+  el: '#app',
+  data: function () {
+  	return {
+  		title: 'Advanced',
+	    subtitle: 'Simple CSS UI framework',
+	    link: 'https://advanceddev.github.io/advanced.github.io/advanced.css'
+  	}
+  }
+})
